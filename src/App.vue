2b4336485c7fdbd669341e478284b5867b2dd3fe
Mert <mertfor4_dev@outlook.com>
2021-04-02 19:17:06 +0300

    Add vue home component.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="navbar-wrapper">
+  <div class="container-fluid">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/posts" class="nav-link">Posts</router-link>
+        </li>
+        <li class="nav-item justify-content-center">
+          <router-link to="/register" class="nav-link">Sign Up</router-link>
+        </li>
+        <li class="nav-item justify-content-center">
+          <router-link to="/login" class="nav-link">Login</router-link>
+        </li>
+      </ul>
+    </nav><br />
+    <transition name="fade">
+      <router-view class="container"></router-view>
+    </transition>
   </div>
+  <FlashMessage :position="'right bottom'"></FlashMessage>
+</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    .fade-enter-active, .fade-leave-active {
+      transition: opacity .5s
+    }
+    .fade-enter, .fade-leave-active {
+      opacity: 0
+    }
+
+    .navbar-wrapper {
+      position: absolute;
+      top: 0;
+      right: 0;
+      width: 100% !important;
+      left: 0;
+      z-index: 20;
+      background-color: #140030;
+      height: 100%;
+    }
+
+    .navbar-wrapper > .container-fluid {
+      padding-right: 0;
+      padding-left: 0;
+      width: inherit;
+    }
+
+    .navbar-wrapper .navbar {
+      padding-right: 15px;
+      padding-left: 15px;
+      width: 100% !important;
+    }
+
+    br {
+      display: none;
+    }
+
+
 </style>
+
+<script>
+    export default{
+      data() {
+        return {
+          backgroundColor: '#1b0042'
+        }
+      }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,66 @@
 import Vue from 'vue'
 import App from './App.vue'
+import FlashMessage from '@smartweb/vue-flash-message';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-Vue.config.productionTip = false
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+
+Vue.use(VueAxios, axios);
+Vue.use(FlashMessage);
+
+
+Vue.config.productionTip = false;
+
+
+
+import HomeComponent from './components/HomeComponent.vue';
+import CreateComponent from './components/CreateComponent.vue';
+import IndexComponent from './components/IndexComponent.vue';
+import EditComponent from './components/EditComponent.vue';
+import RegisterComponent from './components/RegisterComponent.vue';
+import LoginComponent from './components/LoginComponent.vue';
+
+import './assets/global.css';
+
+const routes = [
+  {
+      name: 'home',
+      path: '/',
+      component: HomeComponent
+  },
+  {
+      name: 'create',
+      path: '/create',
+      component: CreateComponent
+  },
+  {
+      name: 'posts',
+      path: '/posts',
+      component: IndexComponent
+  },
+  {
+      name: 'edit',
+      path: '/:id/edit',
+      component: EditComponent
+  },
+  {
+    name: 'register',
+    path: '/register',
+    component: RegisterComponent
+  },
+  {
+    name: 'login',
+    path: '/login',
+    component: LoginComponent
+  }
+];
+
+const router = new VueRouter({ mode: 'history', routes: routes});
+
+
+new Vue(Vue.util.extend({ router }, App)).$mount('#app');
